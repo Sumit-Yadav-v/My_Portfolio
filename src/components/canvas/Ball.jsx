5d1388dbdbf4c6bf9dcf2fc006b72f3b088d98a2
Sumit-Yadav-v <sumit.yydv@gmail.com>
2025-08-17 17:54:30 +0530

    Update Ball.jsx
    fxing code for android

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -10,8 +10,17 @@ import {
 
 import CanvasLoader from "../Loader";
 
-const Ball = (props) => {
-  const [decal] = useTexture([props.imgUrl]);
+const Ball = ({ imgUrl }) => {
+  let decal = null;
+
+  try {
+    // only load if valid
+    if (imgUrl) {
+      [decal] = useTexture([imgUrl]);
+    }
+  } catch (e) {
+    console.warn("Decal texture failed to load:", imgUrl, e);
+  }
 
   return (
     <Float speed={2} rotationIntensity={1} floatIntensity={2}>
@@ -20,43 +29,36 @@ const Ball = (props) => {
       <mesh castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1, 4]} />
         <meshStandardMaterial
-          color='#fff8eb'
+          color="#fff8eb"
           polygonOffset
           polygonOffsetFactor={-5}
           flatShading
         />
-        
-        <Decal
-          position={[0, 0, 1]}
-          rotation={[2 * Math.PI, 0, 6.25]}
-          scale={1}
-          map={decal}
-          flatShading
-        />
+
+        {/* Only render decal if texture loaded */}
+        {decal && (
+          <Decal
+            position={[0, 0, 1]}
+            rotation={[2 * Math.PI, 0, 6.25]}
+            scale={1}
+            map={decal}
+            flatShading
+          />
+        )}
       </mesh>
-      
     </Float>
-    
   );
 };
 
 const BallCanvas = ({ icon }) => {
   return (
-    <Canvas
-      
-      dpr={[1,2]}
-      gl={{ preserveDrawingBuffer: true }}
-    >
-      
+    <Canvas dpr={[1, 2]} gl={{ preserveDrawingBuffer: true }}>
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false} />
         <Ball imgUrl={icon} />
-        
       </Suspense>
-      
       <Preload all />
     </Canvas>
-    
   );
 };
 
