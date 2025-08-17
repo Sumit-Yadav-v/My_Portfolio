f5589e9a3dba297f284455660e27ac8ff817f565
Sumit-Yadav-v <sumit.yydv@gmail.com>
2025-08-17 22:35:47 +0530

    Update Ball.jsx

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,3 +1,5 @@
+import { useLoader } from "@react-three/fiber";
+import { TextureLoader } from "three";
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import {
@@ -7,20 +9,42 @@ import {
   Preload,
   useTexture,
 } from "@react-three/drei";
+import {css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  threejs,
+  typescript
+} from "../../assets";
+
 
 import CanvasLoader from "../Loader";
 
-const Ball = ({ imgUrl }) => {
-  let decal = null;
+// ✅ Preload texture
+useLoader.preload(TextureLoader, css);
+useLoader.preload(TextureLoader, mongodb);
+useLoader.preload(TextureLoader, reactjs);
+useLoader.preload(TextureLoader, tailwind);
+useLoader.preload(TextureLoader, redux);
+useLoader.preload(TextureLoader, figma);
+useLoader.preload(TextureLoader, git);
+useLoader.preload(TextureLoader, html);
+useLoader.preload(TextureLoader, nodejs);
+useLoader.preload(TextureLoader, typescript);
+useLoader.preload(TextureLoader, threejs);
+useLoader.preload(TextureLoader, docker);
+useLoader.preload(TextureLoader, javascript);
 
-  try {
-    // only load if valid
-    if (imgUrl) {
-      [decal] = useTexture([imgUrl]);
-    }
-  } catch (e) {
-    console.warn("Decal texture failed to load:", imgUrl, e);
-  }
+
+const Ball = (props) => {
+  const [decal] = useTexture([props.imgUrl]);
 
   return (
     <Float speed={2} rotationIntensity={1} floatIntensity={2}>
@@ -29,36 +53,43 @@ const Ball = ({ imgUrl }) => {
       <mesh castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1, 4]} />
         <meshStandardMaterial
-          color="#fff8eb"
+          color='#fff8eb'
           polygonOffset
           polygonOffsetFactor={-5}
           flatShading
         />
-
-        {/* Only render decal if texture loaded */}
-        {decal && (
-          <Decal
-            position={[0, 0, 1]}
-            rotation={[2 * Math.PI, 0, 6.25]}
-            scale={1}
-            map={decal}
-            flatShading
-          />
-        )}
+        
+        <Decal
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+          scale={1}
+          map={decal}
+          flatShading
+        />
       </mesh>
+      
     </Float>
+    
   );
 };
 
 const BallCanvas = ({ icon }) => {
   return (
-    <Canvas dpr={[1, 2]} gl={{ preserveDrawingBuffer: true }}>
+    <Canvas
+      
+      dpr={[1,2]}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false} />
         <Ball imgUrl={icon} />
+        
       </Suspense>
+      
       <Preload all />
     </Canvas>
+    
   );
 };
 
